ce489ee9e517b95a5d66135a0164bdae26f1b4c4
Renan Duarte <renandg.axl@gmail.com>
2022-01-11 14:09:46 -0300

    Desafio 01

---
+++ desafio1/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas', '000.000.000-00', 100, TIMESTAMP WITH TIME ZONE '1999-07-13T20:50:07.12345Z', '0');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Leonardo', '111.111.111-11', 200, TIMESTAMP WITH TIME ZONE '1998-07-13T20:50:07.12345Z', '0');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fabiana', '222.222.222-22', 300, TIMESTAMP WITH TIME ZONE '1997-07-13T20:50:07.12345Z', '0');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gabriela', '333.333.333-33', 400, TIMESTAMP WITH TIME ZONE '1996-07-13T20:50:07.12345Z', '0');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Junior', '444.444.444-44', 100, TIMESTAMP WITH TIME ZONE '1995-07-13T20:50:07.12345Z', '0');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Edmundo', '555.555.555-55', 300, TIMESTAMP WITH TIME ZONE '1994-07-13T20:50:07.12345Z', '1');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Cleo', '778.778.78-78', 600, TIMESTAMP WITH TIME ZONE '1993-07-13T20:50:07.12345Z', '0');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vera', '888.888.888-88', 500, TIMESTAMP WITH TIME ZONE '1999-07-13T20:50:07.12345Z', '1');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Alex', '123.123.123-12', 100, TIMESTAMP WITH TIME ZONE '1999-07-13T20:50:07.12345Z', '2');
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria', '456.456.456-45', 100, TIMESTAMP WITH TIME ZONE '1999-07-13T20:50:07.12345Z', '2');
